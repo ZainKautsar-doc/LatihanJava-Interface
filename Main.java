60ccc75da9db74726166ac59ab52f25a6f488cb6
ZainKautsar-doc <zainkautsarridha@gmail.com>
2025-10-28 11:36:15 +0700

    latihan input data implementasi innterface

--- Main.java
+++ Main.java
@@ -4,9 +4,12 @@ class Main {
     public static void main(String[] args) {
         Scanner inputan = new Scanner(System.in);
 
+        System.out.print("Masukkan username: ");
         String username = inputan.nextLine();
+        System.out.print("Masukkan password: ");
         String password = inputan.nextLine();
 
+        System.out.println("\n\nData yang dimasukkan:");
         System.out.println("Username: " + username);
         System.out.println("Password: " + password);
     }
